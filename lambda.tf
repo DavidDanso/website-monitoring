cac2b4828cddd44f57ab2a966d4bedd6395565c1
DavidDanso <desiingerboy@icloud.com>
2026-06-11 15:01:39 +0000

    chore: fix indentation and alignment of lambda configuration attributes

--- lambda.tf
+++ lambda.tf
@@ -50,11 +50,11 @@ resource "aws_cloudwatch_log_group" "lambda_logs" {
 resource "aws_lambda_function" "url_checker" {
   filename         = data.archive_file.lambda_zip.output_path
   function_name    = "website-url-checker"
-  role            = aws_iam_role.lambda_role.arn
-  handler         = "index.lambda_handler"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.lambda_handler"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime         = "python3.11"
-  timeout         = 30
+  runtime          = "python3.11"
+  timeout          = 30
 
   environment {
     variables = {
