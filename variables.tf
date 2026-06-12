4851d63759a24a70b97c392c529089ab3ac8b96c
DavidDanso <desiingerboy@icloud.com>
2026-06-12 11:10:32 +0000

    chore: inject email secret into deployment workflow and remove default values for required terraform variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "email_address" {
@@ -12,11 +11,9 @@ variable "email_address" {
 variable "urls_to_check" {
   description = "List of URLs to monitor"
   type        = list(string)
-  default     = ["https://google.com", "https://aws.amazon.com"]
 }
 
 variable "check_interval_minutes" {
   description = "How often to check URLs (in minutes)"
   type        = number
-  default     = 5
 }
