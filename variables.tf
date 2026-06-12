33d90796c15bf0c7fd480f21afd405b87fa14f5e
DavidDanso <desiingerboy@icloud.com>
2026-06-12 16:33:38 +0000

    feat: implement terraform apply job and define default values for variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "email_address" {
@@ -11,9 +12,11 @@ variable "email_address" {
 variable "urls_to_check" {
   description = "List of URLs to monitor"
   type        = list(string)
+  default     = ["https://google.com", "https://aws.amazon.com"]
 }
 
 variable "check_interval_minutes" {
   description = "How often to check URLs (in minutes)"
   type        = number
+  default = 5
 }
